1d2d4cada3165697f588585f7ec07e27094509b5
LMT Helpdesk <mwatstein@umass.edu>
2024-12-18 17:25:17 +0000

    QA update Dec 18

--- mk_runs.py
+++ mk_runs.py
@@ -43,13 +43,13 @@ on["NGC5899"] = \
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
-pars1["IC3392"]  = "pix_list=-13,14,15  dv=150 dw=250"
-pars1["NGC3338"] = "pix_list=-13,14,15  dv=150 dw=250"   # check
-pars1["NGC3430"] = "pix_list=-13,14,15  dv=150 dw=250"
-pars1["NGC4189"] = "pix_list=-13,14,15  dv=150 dw=250"
-pars1["NGC5012"] = "pix_list=-13,14,15  dv=150 dw=250"   # check
-pars1["NGC5347"] = "pix_list=-13,14,15  dv=150 dw=250 vlsr=2400"
-pars1["NGC5899"] = "pix_list=-13,14,15  dv=350 dw=400 vlsr=2650"
+pars1["IC3392"]  = "pix_list=-13,14,15  dv=150 dw=250 qagrade=3"
+pars1["NGC3338"] = "pix_list=-13,14,15  dv=150 dw=250 qagrade=3"   # check
+pars1["NGC3430"] = "pix_list=-13,14,15  dv=150 dw=250 qagrade=3"
+pars1["NGC4189"] = "pix_list=-13,14,15  dv=150 dw=250 qagrade=3"
+pars1["NGC5012"] = "pix_list=-13,14,15  dv=150 dw=250 qagrade=3"   # check
+pars1["NGC5347"] = "pix_list=-13,14,15  dv=150 dw=250 vlsr=2400 qagrade=3"
+pars1["NGC5899"] = "pix_list=-13,14,15  dv=350 dw=400 vlsr=2650 qagrade=3"
 
 
 #        common parameters per source on subsequent runs (run1b, run2b), e.g. bank=0 for WARES
